803eea31420a54264926e8a08ee439c308e7288b
Eddy Cedeño <miguepoke28@gmail.com>
2022-10-16 11:55:52 -0500

    Move player, create ball and bounce ball

--- Day 21/pong/main.py
+++ Day 21/pong/main.py
@@ -1,8 +1,10 @@
 from turtle import Screen
 from net import Net
 from player import Player
+from ball import Ball
+import time
 
-SCREEN_WIDTH = 600
+SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 SCREEN_COLOR = "black"
 SCREEN_TITLE = "PONG!"
@@ -12,13 +14,39 @@ screen.setup(width=SCREEN_WIDTH, height=SCREEN_HEIGHT)
 screen.bgcolor(SCREEN_COLOR)
 screen.title(SCREEN_TITLE)
 screen.tracer(0)
-player1 = Player("left")
-player2 = Player("right")
 
-player1.draw_player(SCREEN_WIDTH)
-player2.draw_player(SCREEN_WIDTH)
+# Create the players
+l_player = Player("left")
+r_player = Player("right")
+l_player.draw_player(SCREEN_WIDTH)
+r_player.draw_player(SCREEN_WIDTH)
+
 net = Net()
 net.create_net(SCREEN_HEIGHT)
-screen.update()
+game_on = True
+
+ball = Ball()
+
+# Controls
+screen.listen()
+screen.onkeypress(fun=l_player.move_up, key="w")
+screen.onkeypress(fun=r_player.move_up, key="Up")
+screen.onkeypress(fun=l_player.move_down, key="s")
+screen.onkeypress(fun=r_player.move_down, key="Down")
+
+while game_on:
+    screen.update()
+    ball.move()
+    time.sleep(0.1)
+
+    # Detect vertical wall collision
+    if ball.ycor() > int(SCREEN_HEIGHT/2)-20 or ball.ycor() < int(SCREEN_HEIGHT/-2)+20:
+        ball.bounce_y()
+
+    # Detect paddle collision
+    if ball.distance(r_player) < 50 and ball.xcor() > int(SCREEN_WIDTH/2)-50 \
+            or ball.distance(l_player) < 50 and ball.xcor() < (int(SCREEN_WIDTH/2)-50)*-1:
+        ball.bounce_x()
+
 
 screen.exitonclick()
--- Day 21/pong/player.py
+++ Day 21/pong/player.py
@@ -1,24 +1,25 @@
 from turtle import Turtle
 
 
-class Player:
+class Player(Turtle):
     def __init__(self, player_side):
-        self.paddle = Turtle("square")
-        self.paddle.color("white")
-        self.paddle.left(90)
-        self.paddle.turtlesize(stretch_len=3)
+        super().__init__()
+        self.shape("square")
+        self.color("white")
+        self.left(90)
+        self.turtlesize(stretch_len=5)
         self.player_side = player_side
-        self.paddle.penup()
+        self.penup()
 
     def draw_player(self, screen_width):
         """Create the player, request screen width."""
         if self.player_side == 'right':
-            self.paddle.setposition(x=int(screen_width / 2) - 20, y=0)
+            self.setposition(x=int(screen_width / 2) - 30, y=0)
         else:
-            self.paddle.setposition(x=int((screen_width / 2) - 10)*-1, y=0)
+            self.setposition(x=int((screen_width / 2) - 30)*-1, y=0)
 
     def move_up(self):
-        self.paddle.forward(20)
+        self.forward(20)
 
     def move_down(self):
-        self.paddle.forward(-20)
+        self.forward(-20)
