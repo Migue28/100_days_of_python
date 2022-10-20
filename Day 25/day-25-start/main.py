fa223c22442f42c8fa112aa7ce34361dd2573fb3
Eddy Cedeño <miguepoke28@gmail.com>
2022-10-19 22:04:30 -0500

    Read and analyze csv files with pandas

--- Day 25/day-25-start/main.py
+++ Day 25/day-25-start/main.py
@@ -11,12 +11,12 @@
 #         temperature.append(int(row[1]))
 # print(temperature)
 
-import pandas as pd
-
-data = pd.read_csv("weather_data.csv")
-
-# Average temperature
-temps = data["temp"]
+# import pandas as pd
+#
+# data = pd.read_csv("weather_data.csv")
+#
+# # Average temperature
+# temps = data["temp"]
 # average_temp = sum(temps)/len(temps)
 # print(temps)
 # print(round(average_temp, 2))
@@ -27,7 +27,13 @@ temps = data["temp"]
 # monday = data[data.day == "Monday"]
 # print((monday.temp*1.8) + 32)
 
-dict_data = {"Names": ["Ed", "Al", "Amy"],
-             "Scores": [45, 94, 24]}
-data = pd.DataFrame(dict_data)
-data.to_csv("new_csv_file.csv")
+# dict_data = {"Names": ["Ed", "Al", "Amy"],
+#              "Scores": [45, 94, 24]}
+# data = pd.DataFrame(dict_data)
+# data.to_csv("new_csv_file.csv")
+
+import pandas as pd
+
+data = pd.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+color_squirrel_list = data["Primary Fur Color"]
+color_squirrel_list.value_counts().to_csv("color_squirrel_counter.csv")
