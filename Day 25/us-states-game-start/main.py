756457053afb2e42fb57d60a16d0185d677281b4
Eddy Cedeño <miguepoke28@gmail.com>
2022-10-20 19:58:13 -0500

    Create csv of missing states, exit on command

--- Day 25/us-states-game-start/main.py
+++ Day 25/us-states-game-start/main.py
@@ -10,10 +10,13 @@ data = pd.read_csv("50_states.csv")
 # print(data)
 game_is_on = True
 user_points = 0
+user_states = []
 while game_is_on:
-    user_state = screen.textinput(f"{user_points}/{len(data)} States Correct", "What's another state name?")
+    user_state = screen.textinput(f"{user_points}/{len(data)} States Correct", "What's another state name?").title()
+    if user_state == "Exit":
+        break
     for state in data.state:
-        if state.lower() == user_state.lower():
+        if state == user_state:
             # print(f"{state} x: {data[data.state == state].x.item()} y: {data[data.state == state].y.item()}")
             user_points += 1
             state_name = turtle.Turtle()
@@ -21,8 +24,14 @@ while game_is_on:
             state_name.penup()
             state_name.goto(int(data.x[data.state == state].item()), int(data.y[data.state == state].item()))
             state_name.write(state)
-            # screen.update()
+            user_states.append(state)
             break
     if user_points == len(data):
         game_is_on = False
-screen.exitonclick()
+
+missing_states = []
+for state in data.state.to_list():
+    if state not in user_states:
+        missing_states.append(state)
+
+pd.DataFrame(missing_states).to_csv("missing_states.csv")
