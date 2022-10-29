bee8a16cea0b41e563ef4502ea7156e128dbdc31
Eddy Cedeño <miguepoke28@gmail.com>
2022-10-29 17:19:32 -0500

    Save files

--- Day 29/password_manager/main.py
+++ Day 29/password_manager/main.py
@@ -1,14 +1,28 @@
 from tkinter import *
+import pandas as pd
 
 WHITE = "#ffffff"
-# TODO 1: Accept website name, user/email and password from user
+
+
 # TODO 2: Generate random password
+def generate_password():
+    pw = ""
+
+
 # TODO 3: Clip generated password in clipboard to paste easily
-# TODO 4: Save generated account to a file
+# TODO 4: Save generated tuple to a file
+def save_account():
+    web_name = user_web.get()
+    user_name = user_mail.get()
+    password_name = password.get()
+    account_format = f"Web: {web_name} || User: {user_name} || Password: {password_name}\n"
+    with open(mode="a", file="saved_accounts.txt") as file:
+        file.write(account_format)
+
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=100, pady=20, bg=WHITE)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(width=200, height=190, highlightthickness=0)
 logo_image = PhotoImage(file="logo.png")
@@ -16,32 +30,32 @@ canvas.create_image(100, 95, image=logo_image)
 canvas.grid(row=0, column=1)
 
 # TODO 1: Request website name, user/email and password from user
-user_web_label = Label(text="Website: ", padx=10)
-user_web_label.grid(row=2, column=0)
+user_web_label = Label(text="Website:")
+user_web_label.grid(row=1, column=0)
 
-user_web = Entry(width=50)
+user_web = Entry(width=51)
 user_web.insert(END, "")
-user_web.grid(row=2, column=1)
+user_web.grid(row=1, column=1, columnspan=2)
 user_web.focus()
 
-user_mail_label = Label(text="Email/Username: ", padx=10)
-user_mail_label.grid(row=3, column=0)
+user_mail_label = Label(text="Email/Username:")
+user_mail_label.grid(row=2, column=0)
 
-user_mail = Entry(width=50)
-user_mail.insert(END, "")
-user_mail.grid(row=3, column=1)
+user_mail = Entry(width=51)
+user_mail.insert(END, "example@mail")
+user_mail.grid(row=2, column=1, columnspan=2)
 
-password_label = Label(text="Password: ", padx=10)
-password_label.grid(row=4, column=0)
+password_label = Label(text="Password:")
+password_label.grid(row=3, column=0)
 
-password = Entry(width=20)
+password = Entry(width=32)
 password.insert(END, "")
-password.grid(row=4, column=1)
+password.grid(row=3, column=1)
 
-generate_password_button = Button(text="Generate password")
-generate_password_button.grid(row=4, column=2)
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
 
-add_password_button = Button(text="Add", width=50)
-add_password_button.grid(row=5, column=1)
+add_password_button = Button(text="Add", width=43, command=save_account)
+add_password_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
