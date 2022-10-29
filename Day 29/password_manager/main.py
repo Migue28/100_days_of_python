2a7f6174c6dde5b15b363fc5ca022a4a8f755c2e
Eddy Cedeño <miguepoke28@gmail.com>
2022-10-29 14:57:04 -0500

    Create todo list

---
+++ Day 29/password_manager/main.py
@@ -0,0 +1,16 @@
+from tkinter import *
+
+
+# TODO 1: Accept website name, user/email and password from user
+# TODO 2: Generate random password
+# TODO 3: Clip generated password in clipboard to paste easily
+# TODO 4: Save generated account to a file
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx=100, pady=50)
+
+canvas = Canvas()
+
+
+window.mainloop()
