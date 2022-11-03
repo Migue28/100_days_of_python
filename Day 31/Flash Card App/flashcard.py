7562700cd807193020e80c879c5b352aa9dec21f
Eddy Cedeño <miguepoke28@gmail.com>
2022-11-02 20:40:03 -0500

    Show character on screen from data

---
+++ Day 31/Flash Card App/flashcard.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from random import randint
+
+data = pd.read_csv("data/Chinese_to_spanish.csv")
+df = pd.DataFrame(data)
+
+
+class Flashcard:
+    def __init__(self):
+        self.card_index = randint(0, 5)
+        self.wrong_cards = []
+        self.wrong_card_index = 1
+
+    def select_card(self):
+        return df[["Simplified", "G_Meaning"]].iloc[self.card_index]
--- Day 31/Flash Card App/main.py
+++ Day 31/Flash Card App/main.py
@@ -1,17 +1,22 @@
 from tkinter import *
+from flashcard import Flashcard
 
 BACKGROUND_COLOR = "#B1DDC6"
 
 window = Tk()
-window.title("Chinese Flash Card App")
+window.title("Chinese Flashcard App")
 window.config(bg=BACKGROUND_COLOR, pady=50, padx=50)
+flashcard = Flashcard()
+card = flashcard.select_card()
 
 # Card Layout
 canvas = Canvas(width=800, height=526, bg=BACKGROUND_COLOR, highlightthickness=0)
 card_back_image = PhotoImage(file="images/card_back.png")
 card_front_image = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=card_back_image)
+canvas.create_image(400, 263, image=card_front_image)
 canvas.grid(row=0, column=0, columnspan=2)
+canvas.create_text(400, 100, text="Chino", font=("Courier New", 20))
+canvas.create_text(400, 263, text=card["Simplified"], font=("Courier New", 80))
 
 # Buttons layout
 wrong_image = PhotoImage(file="images/wrong.png")
