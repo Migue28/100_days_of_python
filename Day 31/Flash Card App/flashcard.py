ff514aa127dc4b70051adc8f41e3429bc4ed60b5
Eddy Cedeño <miguepoke28@gmail.com>
2022-11-03 13:00:34 -0500

    Add Select and Remove card logic

--- Day 31/Flash Card App/flashcard.py
+++ Day 31/Flash Card App/flashcard.py
@@ -1,19 +1,31 @@
 import pandas as pd
 from random import randint, choice
 
-data = pd.read_csv("data/Chinese_to_spanish.csv")
-df = pd.DataFrame(data)
+_data = pd.read_csv("data/Chinese_to_spanish.csv")
+df = pd.DataFrame(_data)
 
 
 class Flashcard:
-    def __init__(self):
-        self.card_index = randint(0, 5)
-        self.wrong_cards = []
-        self.wrong_card_index = 1
+    def __init__(self, data_quantity):
+        self.data_quantity = data_quantity
+        self.data = df[0:self.data_quantity]
+        self.right_cards = pd.DataFrame()
 
     def select_card(self):
-        return df[["Simplified", "Pinyin", "G_Meaning"]].iloc[self.card_index]
+        try:
+            card_index = randint(0, len(self.data)-1)
+        except ValueError:
+            self.data = self.right_cards
+            self.right_cards = pd.DataFrame()
+            card_index = randint(0, len(self.data) - 1)
+        except IndexError:
+            self.data = self.right_cards
+            self.right_cards = pd.DataFrame()
+            card_index = randint(0, len(self.data) - 1)
+        return self.data[["Simplified", "Pinyin", "G_Meaning"]].iloc[card_index]
 
-
-flash = Flashcard()
-flash.card_with_loc()
+    def remove_right_card(self, row):
+        word_index = self.data[self.data["Simplified"] == row["Simplified"]].index
+        self.right_cards = pd.concat([self.right_cards, self.data.iloc[word_index]], ignore_index=True)
+        self.data = self.data.drop(word_index, inplace=False)
+        self.data.reset_index(drop=True, inplace=True)
--- Day 31/Flash Card App/main.py
+++ Day 31/Flash Card App/main.py
@@ -6,16 +6,15 @@ BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Chinese Flashcard App")
 window.config(bg=BACKGROUND_COLOR, pady=50, padx=50)
-flashcard = Flashcard()
+flashcard = Flashcard(10)
 language = "zh"
 
 
-def change_language():
+def show_card():
     global language
     if language == "zh":
         card_title = "Chino"
         card_word = "Simplified"
-        language = "es"
         image = card_front_image
         pinyin = "Pinyin"
         canvas.itemconfig(canvas_card_pinyin, text=card[f"{pinyin}"])
@@ -23,12 +22,28 @@ def change_language():
     elif language == "es":
         card_title = "Español"
         card_word = "G_Meaning"
-        language = "zh"
         image = card_back_image
         canvas.itemconfig(canvas_card_pinyin, text="")
     canvas.itemconfig(canvas_card_image, image=image)
     canvas.itemconfig(canvas_card_title, text=f"{card_title}")
     canvas.itemconfig(canvas_card_word, text=card[f"{card_word}"].capitalize())
+
+
+def change_language():
+    global language
+    if language == "zh":
+        language = "es"
+    elif language == "es":
+        language = "zh"
+
+
+def change_selected_card():
+    global card, language
+    card = flashcard.select_card()
+    language = "zh"
+    show_card()
+    change_language()
+    window.after(5000, show_card)
 
 
 # Card Layout
@@ -42,17 +57,20 @@ canvas.grid(row=0, column=0, columnspan=2)
 # Buttons layout
 wrong_image = PhotoImage(file="images/wrong.png")
 right_image = PhotoImage(file="images/right.png")
-wrong_button = Button(image=wrong_image, highlightthickness=0)
+wrong_button = Button(image=wrong_image, highlightthickness=0, command=change_selected_card)
 wrong_button.grid(row=1, column=0)
-right_button = Button(image=right_image, highlightthickness=0)
-right_button.grid(row=1, column=1)
+
 
 # Card behavior
 card = flashcard.select_card()
 canvas_card_title = canvas.create_text(400, 100, font=("Courier New", 20))
 canvas_card_pinyin = canvas.create_text(400, 150, font=("Courier New", 20))
 canvas_card_word = canvas.create_text(400, 263, font=("Courier New", 80))
-change_language()
-window.after(5000, change_language)
+show_card()
+window.after(1000, show_card)
+
+# Button behavior
+right_button = Button(image=right_image, highlightthickness=0, command=flashcard.remove_right_card(card))
+right_button.grid(row=1, column=1)
 
 window.mainloop()
